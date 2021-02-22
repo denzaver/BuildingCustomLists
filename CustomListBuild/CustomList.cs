538a56c46f6f244184371e0033e960cfefbcdbb9
Denis Zavertyayev <denisz2277@gmail.com>
2021-02-22 09:41:19 -0800

    Created an AddMethodUnitTest class and linked it with the CustomList class where I will be creating the main methods and logic to test. Set the CustomList class to generic. Created an Add Item and Increate Count By One method taking in a nnumber and then checking to see if the count went up.

--- CustomListBuild/CustomList.cs
+++ CustomListBuild/CustomList.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CustomListBuild
 {
-    class CustomList
+    public class CustomList<T>
     {
     }
 }
---
+++ CustomListUnitTesting/AddMethodUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomListBuild;
+
+namespace CustomListUnitTesting
+{
+    [TestClass]
+    public class AddMethodUnitTest
+    {
+
+        // checking the index of the item on your CustomList<T> to make sure it was added
+        // check the count of the list to see if it was added
+        // check the capacity of our list to make sure it increases when we add an item beyond the original capacity
+        [TestMethod]
+        public void Add_Item_IncreaseCount_ByOne()
+        {
+
+            //Arrange
+            CustomList<int> collectionOfNumbers = new CustomList<int>();
+            int number = 8;
+            int expected = 1;
+            int actual;
+
+            //Act
+            collectionOfNumbers.Add(number);
+            actual = collectionOfNumbers.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+    }
+}
