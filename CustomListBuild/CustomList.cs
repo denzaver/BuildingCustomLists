a2ec9654e8fab466750bd18245a1147d82cf3a02
Denis Zavertyayev <denisz2277@gmail.com>
2021-02-22 14:25:34 -0800

    Created 7 Add test over all. Test to increase capacity to 7, Check Index at 0, Add items to increase counts to 2, Increase count to 5, Check index 4, Check for original numbers after increasing capacity.

--- CustomListBuild/CustomList.cs
+++ CustomListBuild/CustomList.cs
@@ -8,5 +8,39 @@ namespace CustomListBuild
 {
     public class CustomList<T>
     {
+        //member variables
+        private T[] _items;  // this is an array "[]"
+
+        private int count;
+        private int capacity;
+        public int Count { get; }//Properties slideshow Health/health example
+        public int Capacity { get; set; }
+
+        //ctor
+        public CustomList()
+        {
+            count = 0;
+            capacity = 4;
+            _items = new T[capacity];
+        }
+
+        //member methods
+        public void Add(T itemToAdd)
+        {
+            if (count == capacity)
+            {
+                // create new array
+                T[] _items = new T[capacity *= 2];
+                // move items over
+                for (int i = 0; i < count; i++)
+                {
+                    
+                }
+                // new array is _items array
+            }
+            _items[count] = itemToAdd;
+            count++;
+        }
+
     }
 }
--- CustomListUnitTesting/AddMethodUnitTest.cs
+++ CustomListUnitTesting/AddMethodUnitTest.cs
@@ -14,7 +14,6 @@ namespace CustomListUnitTesting
         [TestMethod]
         public void Add_Item_IncreaseCount_ByOne()
         {
-
             //Arrange
             CustomList<int> collectionOfNumbers = new CustomList<int>();
             int number = 8;
@@ -27,7 +26,153 @@ namespace CustomListUnitTesting
 
             //Assert
             Assert.AreEqual(expected, actual);
+        }
 
+        [TestMethod]
+        public void Add_Add5Itesm_IncreaseCapacity_At8()//Duplicate setup: Test 5th thing at index 4, test Count of 5, test to see one original item still persists
+        {
+            //Arrange
+            CustomList<int> collectionofNumbers = new CustomList<int>();
+            int number1 = 1;
+            int number2 = 2;
+            int number3 = 3;
+            int number4 = 4;
+            int number5 = 5;
+            int expected = 8;
+            int actual;
+
+            // Act
+            collectionofNumbers.Add(number1);
+            collectionofNumbers.Add(number2);
+            collectionofNumbers.Add(number3);
+            collectionofNumbers.Add(number4);
+            collectionofNumbers.Add(number5);
+            actual = collectionofNumbers.Capacity;
+          
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void Add_Check_At_IndexOf_0()
+        {
+
+            //Arrange
+            CustomList<int> indexOfNumbers = new CustomList<int>();
+            int number = 7;
+            int expected = 7;
+            int actual;
+
+            // Act
+            indexOfNumbers.Add(number);
+
+            actual = indexOfNumbers[0];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_Items_IncreaseBy_Two()//Ad second item, test count, test index
+        {
+
+            // Arrange
+            CustomList<int> collectionOfNumbers = new CustomList<int>();
+            int number1 = 3;
+            int number2 = 4;
+            int expected = 2;
+            int actual;
+
+            // Act
+            collectionOfNumbers.Add(number1);
+            collectionOfNumbers.Add(number2);
+            actual = collectionOfNumbers.Count;
+
+            // Assert 
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_Items_IncreaseBy_Five()//test Count of 5
+        {
+
+            // Arrange
+            CustomList<int> collectionOfNumbers = new CustomList<int>();
+            int number1 = 6;
+            int number2 = 7;
+            int number3 = 8;
+            int number4 = 9;
+            int number5 = 10;
+            int expected = 5;
+            int actual;
+
+            // Act
+            collectionOfNumbers.Add(number1);
+            collectionOfNumbers.Add(number2);
+            collectionOfNumbers.Add(number3);
+            collectionOfNumbers.Add(number4);
+            collectionOfNumbers.Add(number5);
+            actual = collectionOfNumbers.Count;
+
+            // Assert 
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_Check_At_IndexOf_4() // Test 5th thing at index 4
+        {
+
+            //Arrange
+            CustomList<int> indexOfNumbers = new CustomList<int>();
+            int number1 = 1;
+            int number2 = 2;
+            int number3 = 3;
+            int number4 = 4;
+            int number5 = 5;
+            int expected = 5;
+            int actual;
+
+            // Act
+            indexOfNumbers.Add(number1);
+            indexOfNumbers.Add(number2);
+            indexOfNumbers.Add(number3);
+            indexOfNumbers.Add(number4);
+            indexOfNumbers.Add(number5);
+
+            actual = indexOfNumbers[4];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_CheckToSee_OriginalNumberAfter_IncreasedCapacity() //test to see one original item still persists
+        {
+            // Arrange
+            CustomList<int> indexOfNumbers = new CustomList<int>();
+            int number1 = 1;
+            int number2 = 2;
+            int number3 = 3;
+            int number4 = 4;
+            int number5 = 5;
+            int number6 = 6;
+            int number7 = 7;
+            int expected = 2;
+            int actual;
+
+            //Act 
+
+            indexOfNumbers.Add(number1);
+            indexOfNumbers.Add(number2);
+            indexOfNumbers.Add(number3);
+            indexOfNumbers.Add(number4);
+            indexOfNumbers.Add(number5);
+            indexOfNumbers.Add(number6);
+            indexOfNumbers.Add(number7);
+
+            actual = indexOfNumbers[2];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
 
     }
